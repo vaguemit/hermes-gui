04582694971f7f0ebe3ca320c485a68025f8cf8a
vaguemit <mitparikh1046@gmail.com>
2026-05-20 18:58:20 +0530

    feat: add useHermes convenience hook with memoised client methods

---
+++ src/hooks/useHermes.ts
@@ -0,0 +1,19 @@
+import { useCallback } from 'react'
+import { useHermesClient } from '../lib/hermes'
+
+// Thin hook that memoises frequently-used client methods.
+// Components needing the full client can call useHermesClient() directly.
+export function useHermes() {
+  const client = useHermesClient()
+
+  return {
+    client,
+    startGateway: useCallback(() => client.startGateway(), [client]),
+    stopGateway: useCallback(() => client.stopGateway(), [client]),
+    getHealth: useCallback(() => client.getHealth(), [client]),
+    getInstallStatus: useCallback(() => client.getInstallStatus(), [client]),
+    detectApiKeys: useCallback(() => client.detectApiKeys(), [client]),
+    checkUpdate: useCallback(() => client.checkUpdate(), [client]),
+    runDoctor: useCallback(() => client.runDoctor(), [client]),
+  }
+}
